5a216f0eee180c156c708f51163387df58b6d8e1
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-11-24 13:15:56 +0100

    add FriendList

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import { List } from './FriendList.styled';
+
+const FriendList = ({ friends }) => {
+  return (
+    <List className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </List>
+  );
+};
+
+export default FriendList;
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
